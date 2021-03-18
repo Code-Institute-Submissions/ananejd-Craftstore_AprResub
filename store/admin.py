470adc5fd8a29258a10dd0eef2f8cc8c034671b0
ananejd <ananejd@gmail.com>
2021-03-18 15:58:30 +0000

    superuser created and db populated

--- store/admin.py
+++ store/admin.py
@@ -16,3 +16,5 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'slug', 'price',
                     'in_stock', 'created', 'updated']
     list_filter = ['in_stock', 'is_active']
+    list_editable = ['price', 'in_stock']
+    prepopulated_fields = {'slug': ('title',)}
