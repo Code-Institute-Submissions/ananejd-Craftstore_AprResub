dd88b22502be2284ace43faea15d48e229534e88
ananejd <ananejd@gmail.com>
2021-03-18 22:12:08 +0000

    testing models

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -8,6 +8,20 @@ class TestCategoriesModel(TestCase):
     def setUp(self):
         self.data1 = Category.objects.create(name='django', slug='django')
 
+
     def test_category_model_entry(self):
         data = self.data1
         self.assertTrue(isinstance(data, Category))
+
+
+    def test_category_model_entry(self):
+        """
+        Test Category model defaul name
+        """
+        data = self.data1
+        self.assertEqual(str(data), 'django')
+
+
+class TestProductsModel(TestCase):
+    def setUp(self):
+        Category.objects.create(name='django', slug='django')
