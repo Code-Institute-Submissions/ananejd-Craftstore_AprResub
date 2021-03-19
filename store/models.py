bda7b6f7c0191e99f49334113d9a6d697cf484a1
ananejd <ananejd@gmail.com>
2021-03-19 12:00:39 +0000

    fixing categories

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -9,6 +10,10 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
+
 
     def __str__(self):
         return self.name
@@ -34,5 +39,9 @@ class Product(models.Model):
         ordering = ('-created',)
 
 
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug])
+ 
+
     def __str__(self):
-        return self.title
+        return self.title  
